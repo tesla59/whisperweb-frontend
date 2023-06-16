ec6a9c74be98c9716d23fd0d13b54881b930a669
tesla59 <nishant@heim.id>
2023-06-16 20:54:39 +0530

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,16 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <div className='home-box'>
+        <div className='greeting-box'>
+          <p style={{ fontSize: '4rem', color: 'orange' }}>Welcome to WhisperWeb</p>
+          Wanna confess something?
+        </div>
+        <button className='home-button'>
+          Lets Go
+        </button>
+      </div>
+    </>
   )
 }
