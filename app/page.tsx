5cb54da06979a212259bc8772d84b8d8f57ea107
tesla59 <nishant@heim.id>
2023-06-21 18:32:11 +0530

    update some css

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <div className='home-box'>
         <div className='greeting-box'>
-          <p style={{ fontSize: '4rem', color: 'orange' }}>Welcome to WhisperWeb</p>
+          <p style={{ fontSize: '4rem', color: 'orange' }}><b>Welcome to WhisperWeb</b></p>
           <p style={{ color: '#000080' }}>Wanna confess something?</p>
         </div>
         <Link href={'/confess'} className='home-button'>
